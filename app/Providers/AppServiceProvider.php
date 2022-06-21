f625afd152512f634299667d437d8f4bbed86a5b
Ahmed Almory <ahmedjoda02@gmail.com>
2022-06-21 14:19:52 +0200

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,35 +8,30 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-   /**
-    * Bootstrap any application services.
-    *
-    * @return void
-    */
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
 
-   public function boot()
-   {
+    public function boot()
+    {
+        if (file_exists(base_path('config/itconfiguration.php'))) {
+            Schema::defaultStringLength(config('itconfiguration.SchemadefaultStringLength'));
+            if (config('itconfiguration.ForeignKeyConstraints')) {
+                Schema::enableForeignKeyConstraints();
+            } else {
+                Schema::disableForeignKeyConstraints();
+            }
+        }
+    }
 
-      if (file_exists(base_path('config/itconfiguration.php'))) {
-         Schema::defaultStringLength(config('itconfiguration.SchemadefaultStringLength'));
-         if (config('itconfiguration.ForeignKeyConstraints')) {
-            Schema::enableForeignKeyConstraints();
-         } else {
-            Schema::disableForeignKeyConstraints();
-         }
-      }
-   }
-
-   /**
-    * Register any application services.
-    *
-    * @return void
-    */
-   public function register()
-   {
-      app()->bind('path.public', function () {
-         return base_path('public_html');
-      });
-
-   }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
 }
