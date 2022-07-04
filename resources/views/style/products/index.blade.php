bdbecc6e1c0ed835519ef60668f9dbccc4603eca
ahmedtofaha10 <ahmedtofaha999@gmail.com>
2022-07-04 14:16:03 +0300

    dd

--- resources/views/style/products/index.blade.php
+++ resources/views/style/products/index.blade.php
@@ -162,5 +162,5 @@
     {!! Form::close() !!}
 @stop
 
-<!-- {!! modelBox("addproduct","اضافة الخدمة","box_add","status_add","add_product") !!} -->
-<!-- {!! modelBox("editproduct","تغير الخدمة","box_edit","status_edit","edit_product") !!} -->
+ {!! modelBox("addproduct","اضافة الخدمة","box_add","status_add","add_product") !!} 
+ {!! modelBox("editproduct","تغير الخدمة","box_edit","status_edit","edit_product") !!} 
