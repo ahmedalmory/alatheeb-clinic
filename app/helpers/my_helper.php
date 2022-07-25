21628bf572171064893c0c935c1a676588b4cb85
AhmedTofaha <42559476+ahmedtofaha10@users.noreply.github.com>
2022-07-25 20:00:21 +0200

    dd

--- app/helpers/my_helper.php
+++ app/helpers/my_helper.php
@@ -19,7 +19,6 @@ function modelBoxLarge($modal_id = '', $boxTitle = '', $boxid, $status_msg = '',
 function patient_name($id)
 {
     if ($id) {
-
         return DB::table('patients')->where('id', $id)->first()->first_name;
     } else {
         echo "";
